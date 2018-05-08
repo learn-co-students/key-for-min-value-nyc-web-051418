4e678b90004707de1fc53b94ab73f8c5b650c5ce
Tony Ho <ho.tonym@gmail.com>
2018-05-08 22:01:07 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,11 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+   if name_hash.length > 0
+     new_array = name_hash.max_by(name_hash.length){|x,y| y }
+     new_array.reverse!
+     new_array[0][0]
+   else
+     nil
+   end
 end
