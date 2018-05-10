76000620089ab0ff0f00c2db2676d0fa2873fc17
GregS30 <gjschmitt@live.com>
2018-05-10 08:15:08 -0400

    lesson done

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,19 @@
 
 def key_for_min_value(name_hash)
 
+  min_key = nil
+  min_value = 0
+
+  name_hash.each do |key, value|
+    if min_key == nil
+      min_value = value
+      min_key = key
+    elsif value < min_value
+      min_value = value
+      min_key = key
+    end
+  end
+
+  min_key
+
 end
